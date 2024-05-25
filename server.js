4d089e11b07f57c882fdcb5d1faafaadb20b75bf
faisalsalam <fsenterprise1@gmail.com>
2024-05-25 14:20:00 -0400

    updated directory structure

---
+++ server.js
@@ -0,0 +1,194 @@
+const express = require('express');
+// Import and require Pool (node-postgres)
+// We'll be creating a Connection Pool. Read up on the benefits here: https://node-postgres.com/features/pooling
+const { Pool } = require('pg');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Connect to database
+const pool = new Pool(
+  {
+    // TODO: Enter PostgreSQL username
+    user: 'postgres',
+    // TODO: Enter PostgreSQL password
+    password: 'develop',
+    host: 'localhost',
+    database: 'employees'
+  },
+  console.log(`Connected to the employees database.`)
+)
+
+pool.connect();
+
+// Create a department
+app.post('/api/new-department', ({ body }, res) => {
+  const sql = `INSERT INTO department (id, department_name)
+    VALUES ($1), ($2)`;
+  const params = [body.id, body.dept_name];
+
+  pool.query(sql, params, (err, result) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: 'success',
+      data: body
+    });
+  });
+});
+
+// Create a role
+app.post('/api/new-role', ({ body }, res) => {
+  const sql = `INSERT INTO role (id, title, salary, department_id)
+    VALUES ($1), ($2), ($3), ($4)`;
+  const params = [body.id, body.title, body.salary, body.department_id];
+
+  pool.query(sql, params, (err, result) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: 'success',
+      data: body
+    });
+  });
+});
+// Create an employee
+app.post('/api/new-employee', ({ body }, res) => {
+  const sql = `INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+    VALUES ($1), ($2), ($3), ($4), ($5)`;
+  const params = [body.id, body.first_name, body.last_name, body.role_id, body.manager_id];
+
+  pool.query(sql, params, (err, result) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: 'success',
+      data: body
+    });
+  });
+});
+// Read all departments
+app.get('/api/department', (req, res) => {
+  const sql = `SELECT * from department`;
+
+  pool.query(sql, (err, { rows }) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: 'success',
+      data: rows
+    });
+  });
+});
+
+// Read all role
+app.get('/api/role', (req, res) => {
+  const sql = `SELECT * from role`;
+
+  pool.query(sql, (err, { rows }) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: 'success',
+      data: rows
+    });
+  });
+});
+
+// Read all employee
+app.get('/api/employee', (req, res) => {
+  const sql = `SELECT * from employee`;
+
+  pool.query(sql, (err, { rows }) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: 'success',
+      data: rows
+    });
+  });
+});
+
+// Delete a 
+// app.delete('/api/movie/:id', (req, res) => {
+//   const sql = `DELETE FROM movies WHERE id = $1`;
+//   const params = [req.params.id];
+
+//   pool.query(sql, params, (err, result) => {
+//     if (err) {
+//       res.statusMessage(400).json({ error: err.message });
+//     } else if (!result.rowCount) {
+//       res.json({
+//         message: 'Movie not found'
+//       });
+//     } else {
+//       res.json({
+//         message: 'deleted',
+//         changes: result.rowCount,
+//         id: req.params.id
+//       });
+//     }
+//   });
+// });
+
+// Read list of all employees and associated employee name using LEFT JOIN
+// app.get('/api/movie-reviews', (req, res) => {
+//   const sql = `SELECT movies.movie_name AS movie, reviews.review FROM reviews LEFT JOIN movies ON reviews.movie_id = movies.id ORDER BY movies.movie_name;`;
+//   pool.query(sql, (err, { rows }) => {
+//     if (err) {
+//       res.status(500).json({ error: err.message });
+//       return;
+//     }
+//     res.json({
+//       message: 'success',
+//       data: rows
+//     });
+//   });
+// });
+
+// BONUS: Update role
+app.put('/api/role/:id', (req, res) => {
+  const sql = `UPDATE role SET title = $1 WHERE id = $2`;
+  const params = [req.body.title, req.params.id];
+
+  pool.query(sql, params, (err, result) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+    } else if (!result.rowCount) {
+      res.json({
+        message: 'role not found'
+      });
+    } else {
+      res.json({
+        message: 'success',
+        data: req.body,
+        changes: result.rowCount
+      });
+    }
+  });
+});
+
+// Default response for any other request (Not Found)
+app.use((req, res) => {
+  res.status(404).end();
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
